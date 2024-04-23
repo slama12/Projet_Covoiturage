ea5b86c98e8c23bb6e04ef5ead4d04a5ec9b6c68
HamzaSeyfu <165190579+HamzaSeyfu@users.noreply.github.com>
2024-04-23 16:40:09 +0200

    Update getUsers.php

--- getUsers.php
+++ getUsers.php
@@ -1,20 +1,20 @@
 <?php
-$host = 'localhost';
-$port = '5432';
-$dbname = 'Projet';
-$user = 'postgres';
-$password = '';
-
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
-$pdo = new PDO($dsn);
+include 'db_connect.php';
 
 $sql = "SELECT u.id_utilisateur AS id, u.nom_utilisateur || ' ' || u.prenom_utilisateur AS name, r.libellerole AS role 
 FROM utilisateur u
 LEFT JOIN affecte a ON u.id_utilisateur = a.id_utilisateur
 LEFT JOIN role r ON r.libellerole = r.libellerole;";
-$stmt = $pdo->prepare($sql);
-$stmt->execute();
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$result = pg_query($dbconn, $sql);
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+$users = pg_fetch_all($result);
 echo json_encode($users);
+
+pg_close($dbconn);
 ?>
+
