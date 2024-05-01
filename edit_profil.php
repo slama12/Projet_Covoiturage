2931734daf60fac3b19c5a296e5049efd4930422
Eyaamk <153676368+Eyaamk@users.noreply.github.com>
2024-05-01 21:31:18 +0200

    edit_profil.php

--- edit_profil.php
+++ edit_profil.php
@@ -10,45 +10,44 @@ if(!isset($_SESSION['user_id'])) {
 include_once "config/db.php";
 
 // Récupérer les informations de l'utilisateur à partir de la base de données
-$user_id = $_SESSION['user_id'];
-$query = "SELECT * FROM users WHERE id = ?";
+$id_utilisateur = $_SESSION['id_utilisateur'];
+$query = "SELECT * FROM utilisateur WHERE id = ?";
 $stmt = $pdo->prepare($query);
-$stmt->execute([$user_id]);
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
+$stmt->execute([$id_utilisateur]);
+$utilisateur = $stmt->fetch(PDO::FETCH_ASSOC);
 
 // formulaire de modification de profil
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // champs du formulaire
-    $username = $_POST['username'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
     $email = $_POST['email'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $phone = $_POST['phone'];
+    $telephone = $_POST['telephone'];
 
     // Mettre à jour les informations de l'utilisateur dans la base de données
-    $query = "UPDATE users SET username = ?, email = ?, firstname = ?, lastname = ?, phone = ? WHERE id = ?";
+    $query = "UPDATE utilisateur SET  nom = ?, prenom = ?, email = ?, telephone = ? WHERE id = ?";
     $stmt = $pdo->prepare($query);
-    $stmt->execute([$username, $email, $firstname, $lastname, $phone, $user_id]);
+    $stmt->execute([ $nom, $prenom,$email, $telehone, $id_utilisateur]);
 
     // si l'utilisateur souhaite modifier le mot de passe
-    if(!empty($_POST['old_password']) && !empty($_POST['new_password'])) {
-        $old_password = $_POST['old_password'];
-        $new_password = $_POST['new_password'];
+    if(!empty($_POST['ancien_mot_de_passe']) && !empty($_POST['nouveau_mot_de_passe'])) {
+        $ancien_mot_de_passe = $_POST['ancien_mot_de_passe'];
+        $nouveau_mot_de_passe = $_POST['nouveau_mot_de_passe'];
 
         // Vérifier si l'ancien mot de passe est correct
-        $query = "SELECT password FROM users WHERE id = ?";
+        $query = "SELECT mot_de_passe FROM utilisateur WHERE id = ?";
         $stmt = $pdo->prepare($query);
-        $stmt->execute([$user_id]);
+        $stmt->execute([$id_utilisateur]);
         $stored_password = $stmt->fetchColumn();
 
-        if(password_verify($old_password, $stored_password)) {
+        if(password_verify($ancien_mot_de_passe, $stored_password)) {
             // si Le mot de passe actuel est correct, hasher le nouveau mot de passe
-            $hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $hashed_password = password_hash($nouveau_mot_de_psse, PASSWORD_DEFAULT);
   
             // Mettre à jour le mot de passe dans la base de données
-            $query = "UPDATE users SET password = ? WHERE id = ?";
+            $query = "UPDATE utilisateurs SET mot_de_passe = ? WHERE id = ?";
             $stmt = $pdo->prepare($query);
-            $stmt->execute([$hashed_password, $user_id]);
+            $stmt->execute([$hashed_password, $id_utilisateur]);
 
             // Rediriger vers la page de profil après la modification
             header("Location: profile.php");
