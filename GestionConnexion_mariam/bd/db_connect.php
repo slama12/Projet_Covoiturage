e6a8ae6b32d66230fbd5795474317f043ab2436e
Mariam Elkhatyb <mariam.elkhatyb2000@gmail.com>
2024-05-02 13:03:17 +0200

    changement du nom du fichier

--- GestionConnexion_mariam/bd/db_connect.php
+++ GestionConnexion_mariam/bd/db_connect.php
@@ -4,7 +4,6 @@ $port = "5432";       // PostgreSQL port
 $dbname = "web_php";    // Database name
 $user = "postgres";  // Database username
 $password = "postgres";  // Database password
-$TRY = "TRY";  // Database password
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
 $dbconn = pg_connect($conn_string);
